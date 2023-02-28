f5c945cedd157eff060bc84c48a73007da27ee79
ayowilfred95 <38457345+ayowilfred95@users.noreply.github.com>
2023-02-28 01:00:07 +0100

    dependencies
    install react-youtube ,movie-trailer using npm

--- src/componenets/Row.js
+++ src/componenets/Row.js
@@ -1,12 +1,15 @@
 import React,{useEffect, useState} from 'react'
 import './Row.css';
 import axios from '../axios';
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title,fetchUrl,isLargeRow}) {
     const [movies, setMovies] = useState([]);
+    const[trailerUrl, setTrailerUrl] = useState("");
 
     // A snippet of code which runs based on a specific condition/variable
     //Run a piece of code when the row loads i.e make a request from tmdb
@@ -25,8 +28,27 @@ function Row({title,fetchUrl,isLargeRow}) {
        fetchData();
     },[fetchUrl]);
 
-   console.log(movies); 
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
 
+      const handleClick= (movie)=>{
+        if(trailerUrl){
+          setTrailerUrl('');
+        }else {
+          movieTrailer(movie?.name || "")
+          .then((url) =>{
+            const urlParams =  new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'));
+          })
+          .catch((error) => console.log(error.message));
+        }
+      }
 
 
   return (
@@ -40,10 +62,12 @@ function Row({title,fetchUrl,isLargeRow}) {
              {movies.map(movie => (
                 <img 
                 key={movie.id}
+                onClick={()=> handleClick(movie)}
                 className={`row_poster ${isLargeRow  && "row_posterLarge"}`}
                 src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} alt={movie.name} />
              ))}
         </div>
+        {trailerUrl  && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
